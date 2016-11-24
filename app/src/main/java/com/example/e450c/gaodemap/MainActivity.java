6dff2ef98acfa6a9136295f8a3c777e633230e94
yujunhua93 <1360743958@qq.com>
2016-11-24 09:13:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/e450c/gaodemap/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.e450c.gaodemap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MapView mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        AMap aMap = mapView.getMap();
+    }
+}
